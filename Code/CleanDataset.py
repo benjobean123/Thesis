3483fcc327407cd04dc28c70c36061285bb62fd2
benjobean123 <benjamin.caleb.davis@gmail.com>
2025-06-10 19:57:24 -0500

    added other excel input

--- Code/CleanDataset.py
+++ Code/CleanDataset.py
@@ -5,7 +5,7 @@ from numpy import nan
 xls1 = pd.ExcelFile("dataset/discourse_analysis_one.xlsx")
 xls2 = pd.ExcelFile("dataset/discourse_analysis_two.xlsx")
 xls3 = pd.ExcelFile("dataset/High_Functioning_Scrum_Communication_Final_v5.xlsx")
-
+xls4 = pd.ExcelFile("dataset/Medium_Functioning_Scrum_Communication_Final_v5.xlsx")
 
 
 ## Read in a sheet with no expected header for each sheet in the excel
