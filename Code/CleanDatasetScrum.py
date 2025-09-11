d82e4b380fca0b5dc5b201aa5db14e268e769873
benjobean123 <60797740+benjobean@users.noreply.github.com>
2025-09-11 09:05:20 -0500

    updated data cleaning

--- Code/CleanDatasetScrum.py
+++ Code/CleanDatasetScrum.py
@@ -6,7 +6,7 @@ import re
 
 xls3 = pd.ExcelFile("dataset/High_Functioning_Scrum_Communication_with_TSA.xlsx")
 xls4 = pd.ExcelFile("dataset/Medium_Functioning_Scrum_Communication_with_TSA.xlsx")
-
+pd.set_option('display.max_columns', None)
 
 
 ## Read in a sheet for each sheet in the excel for scrum simulations
@@ -14,6 +14,10 @@ xls4 = pd.ExcelFile("dataset/Medium_Functioning_Scrum_Communication_with_TSA.xls
 sims_3 = [pd.read_excel(xls3, sheet) for sheet in xls3.sheet_names]
 metrics_3 = sims_3.pop(5) ## Remove the Metrics sheet from the dataframe
 sim_names3 = xls3.sheet_names[1:]
+#print(sims_3)
+
+
+
 
 # Read in the Scrum high functioning excel sheet
 sims_4 = [pd.read_excel(xls4, sheet) for sheet in xls4.sheet_names]
@@ -47,6 +51,7 @@ def clean_sim_scrum_data(sim_df):
     'Sofia': 7, 
     'Zara': 8
   }
+
   sim_df['Name'] = sim_df['Name'].map(team_names)
 
 
@@ -57,8 +62,21 @@ def clean_sim_scrum_data(sim_df):
   column_order = ['Name', 'Timestamp', 'Message', 'Sprint', 'TSA']
   sim_df = sim_df[column_order]
 
-  pd.set_option('display.max_columns', None)
-  #print(sim_df)
+  ## Reorganize the sheets to separate Agile Ceremonies by sprint 
+  # Check for what sprint the line belongs too
+  for line in sim_df['Sprint']:
+    # If sprint 1, add the line to the new sim file
+    match line:
+      case '1':
+        sprint1 = pd.concat([sprint1, pd.sim_df([line])])
+      case '2':
+        sprint2 = pd.concat([sprint2, pd.sim_df([line])])
+      case '3':
+        sprint3 = pd.concat([sprint2, pd.sim_df([line])])
+      case '4':
+        sprint4 = pd.concat([sprint2, pd.sim_df([line])])
+    
+  print(sprint1)
   
 
   return sim_df
@@ -72,8 +90,8 @@ for i in range(len(sims_3)):
     clean_scrum_sims[sim_names3[i]] = clean_sim_scrum_data(sims_3[i])
   except Exception as e:
     print("\n")
-    print(i, sim_names3[i], e)
-    print(sims_3[i])
+    #print(i, sim_names3[i], e)
+    #print(sims_3[i])
 
 # clean the agile sims high functioning
 for i in range(len(sims_4)):
@@ -81,8 +99,8 @@ for i in range(len(sims_4)):
     clean_scrum_sims[sim_names4[i]] = clean_sim_scrum_data(sims_4[i])
   except Exception as e:
     print("\n")
-    print(i, sim_names4[i], e)
-    print(sims_4[i])
+    #print(i, sim_names4[i], e)
+    #print(sims_4[i])
 
 
 
